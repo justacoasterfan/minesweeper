06e9ce64fc8b3d8ce617aafab613263cdbe523ac
justacoasterfan <marlonbruns184@gmail.com>
2023-06-25 14:56:28 +0200

    Whole bunch of new stuff

--- src/main/java/com/triagetrio/minesweeper/App.java
+++ src/main/java/com/triagetrio/minesweeper/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * JavaFX App
@@ -17,9 +18,16 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 600, 600);
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
+        stage.setTitle("Minesweeper");
+
+        PrimaryController primaryController = new PrimaryController();
+
+        primaryController.initBoard();
+        populate();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -33,6 +41,56 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void populate() { 
+        Random random = new Random();
+
+        int repeat = 0;
+        while (repeat < 10 )   {    
+            repeat ++;
+            int x =  random.nextInt(9);
+            int y = random.nextInt(9);
+            if (map [x][y] == true)
+                repeat --;
+            else
+                map [x][y] = true;  
+       /*Risiko für Endlosschleife wenn immer wieder Zahlen 
+       von bereits belegtem Feld gewählt werden */
+     
+        }  
+    }  
+
+    public static boolean map[][] = new boolean[9][9];
+
+    public static byte proximityBombNumber(int x, int y) {
+        //check 8 surrounding fields for bombs
+        byte numberBombs = 0;
+        if (map[x-1][y+1] == true) numberBombs ++;
+        if (map[x+0][y+1] == true) numberBombs ++;
+        if (map[x+1][y+1] == true) numberBombs ++;
+        if (map[x-1][y+0] == true) numberBombs ++;
+        if (map[x+1][y+0] == true) numberBombs ++;
+        if (map[x-1][y-1] == true) numberBombs ++;
+        if (map[x+0][y-1] == true) numberBombs ++;
+        if (map[x+1][y-1] == true) numberBombs ++;
+
+        return numberBombs;
+    }
+
+    public static boolean hasBomb(int x, int y){
+   
+        if (map [x][y] == true)
+            return true;
+        else
+            return false;
+    }
+
+    public static void showEndScreen (boolean hasWon){
+        if (hasWon == true)
+            System.out.println ("Your win are belong to you");
+        else
+            System.out.println ("*boom* You have perished");
     }
 
 }
