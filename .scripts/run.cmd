351b1442bff9afd644c1b01d25434c9f5357f55b
justacoasterfan <marlonbruns184@gmail.com>
2023-06-27 01:53:03 +0200

    Moved VSC jar run script to .scripts

---
+++ .scripts/run.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\marlo\source\repos\minesweeper-java\minesweeper
+call "C:\Program Files\Java\apache-maven-3.9.3\bin\mvn" install
+call java -jar target\minesweeper-1.0-SNAPSHOT-shaded.jar
